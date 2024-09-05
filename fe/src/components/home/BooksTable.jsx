14ad38305abc9d57437c280300de81cee1b047e5
trinkles <cmc0234@dlsud.edu.ph>
2024-09-05 20:27:14 +0800

    fixed css

--- fe/src/components/home/BooksTable.jsx
+++ fe/src/components/home/BooksTable.jsx
@@ -11,7 +11,7 @@ const BooksTable = ({ books }) => {
                     <th className='border border-slate-600 rounded-md'>No</th>
                     <th className='border border-slate-600 rounded-md'>Title</th>
                     <th className='border border-slate-600 rounded-md max-md:hidden'>Author</th>
-                    <th className='border border-slate-600 rounded-md max-md-hidden'>Publish Year</th>
+                    <th className='border border-slate-600 rounded-md max-md:hidden'>Publish Year</th>
                     <th className='border border-slate-600 rounded-md'>Options</th>
                 </tr>
             </thead>
